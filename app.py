426da13b15f0996cc0032ccf789c446260b20703
Jo Emil Holen <jo-emil@outlook.com>
2017-11-26 21:32:06 +0100

    Rename to ledmastree

--- app.py
+++ app.py
@@ -9,35 +9,35 @@ APP = Flask(__name__)
 
 CONTROLLER = LEDController()
 
-@APP.route('/lampmastree/api/v1/leds', methods=['GET'])
+@APP.route('/ledmastree/api/v1/leds', methods=['GET'])
 def get_leds():
     return jsonify(CONTROLLER.get_all())
 
-@APP.route('/lampmastree/api/v1/leds/<int:led_id>', methods=['GET'])
+@APP.route('/ledmastree/api/v1/leds/<int:led_id>', methods=['GET'])
 def get_led(led_id):
     return jsonify(CONTROLLER.get(led_id))
 
-@APP.route('/lampmastree/api/v1/toggle', methods=['GET'])
+@APP.route('/ledmastree/api/v1/toggle', methods=['GET'])
 def toggle_leds():
     return jsonify(CONTROLLER.toggle_all())
 
-@APP.route('/lampmastree/api/v1/toggle/<int:led_id>', methods=['GET'])
+@APP.route('/ledmastree/api/v1/toggle/<int:led_id>', methods=['GET'])
 def toggle_led(led_id):
     return jsonify(CONTROLLER.toggle(led_id))
 
-@APP.route('/lampmastree/api/v1/on', methods=['GET'])
+@APP.route('/ledmastree/api/v1/on', methods=['GET'])
 def on_leds():
     return jsonify(CONTROLLER.on_all())
 
-@APP.route('/lampmastree/api/v1/on/<int:led_id>', methods=['GET'])
+@APP.route('/ledmastree/api/v1/on/<int:led_id>', methods=['GET'])
 def on_led(led_id):
     return jsonify(CONTROLLER.on(led_id))
 
-@APP.route('/lampmastree/api/v1/off', methods=['GET'])
+@APP.route('/ledmastree/api/v1/off', methods=['GET'])
 def off_leds():
     return jsonify(CONTROLLER.off_all())
 
-@APP.route('/lampmastree/api/v1/off/<int:led_id>', methods=['GET'])
+@APP.route('/ledmastree/api/v1/off/<int:led_id>', methods=['GET'])
 def off_led(led_id):
     return jsonify(CONTROLLER.off(led_id))
 
