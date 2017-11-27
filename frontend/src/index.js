1c0c4ec289a0084cd76b0781334d42fbd009fc2c
Jo Emil Holen <jo-emil@outlook.com>
2017-11-27 21:43:33 +0100

    Work on the React implementation

---
+++ frontend/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { AppContainer } from 'react-hot-loader'
+import App from './js/containers/app';
+/*import configureStore from './js/redux/configureStore';
+
+const store = configureStore();*/
+
+const root = document.querySelector('#app');
+ReactDOM.render(
+    <AppContainer>
+        <App/>
+    </AppContainer>,
+    root,
+);
+
+// Webpack Hot Module Replacement API
+if (module.hot) {
+    module.hot.accept('./js/containers/app', () => { render(App) })
+}
---
+++ frontend/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: [
+        'react-hot-loader/patch',
+        './src/index.js',
+    ],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [ 'style-loader', 'css-loader' ]
+            }
+        ]
+    },
+    devServer: {
+        hot: true,
+        contentBase: path.resolve(__dirname, 'dist')
+    }
+};
