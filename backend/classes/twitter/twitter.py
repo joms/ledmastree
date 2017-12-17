9759f3baeaba64e9b15e4027765f83d435257b6e
Jo Emil Holen <jo-emil@outlook.com>
2017-12-17 16:32:41 +0100

    Implement twitter stream support

--- backend/classes/twitter/twitter.py
+++ backend/classes/twitter/twitter.py
@@ -1,3 +1,4 @@
+"""Twitter support controller"""
 import os
 import threading
 from .stream import Stream
@@ -13,12 +14,23 @@ OAUTH_TOKEN_SECRET = os.environ.get('OAUTH_TOKEN_SECRET')
 TERMS = '#Christmas'
 
 class Twitter:
+    """Twitter controller class"""
     def __init__(self, led_list):
-        self.stream = Stream(led_list)
+        self.event_thread = False
+        self.thread = False
+        self.led_list = led_list
 
     def start_stream(self):
-        self.stream.enabled = True
-        
+        """Start Twitter stream"""
+        if self.event_thread:
+            self.event_thread.clear()
+
+        self.event_thread = threading.Event()
+        self.thread = threading.Thread(target=Stream, args=(self.event_thread, APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET, self.led_list, TERMS))
+        self.thread.start()
+
     def stop_stream(self):
-        self.stream.enabled = False
+        """Stop Twitter stream"""
+        if self.event_thread:
+            self.event_thread.set()
     
