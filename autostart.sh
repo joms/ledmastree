9e496b633de115f27335960058a129284d4df5a1
Jo Emil Holen <jo-emil@outlook.com>
2018-12-02 22:02:14 +0100

    fix startup script, and make a cool boot animation

--- autostart.sh
+++ autostart.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-git clone
-cd /home/pi/ledmastree/frontend
-npm install && npm build
-export FLASK_APP=/home/pi/ledmastree/backend/app.py
+git pull
+export FLASK_APP=/home/pi/code/ledmastree/backend/app.py
 python3 -m flask run --host=0.0.0.0
